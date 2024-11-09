c20c22d4dd31e3ce9bf6c09ec5aa5d7f30032918
Piyush arya <piyush.arya@letzcreate.in>
2024-11-10 03:13:21 +0530

    XcountriesSearch

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
 import { useEffect, useState } from 'react';
-import './styles.css'
+import './styles.css';
 
 function App() {
-  // let country=[1,2,3,4,5,6,7,8,9]
-  const [data,setData]=useState([]);
-  const [error, setError] = useState(null);
-  const API_ENDPOINT='https://restcountries.com/v3.1/all';
-  async function fetchApi(){
+  const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]); 
+  const API_ENDPOINT = 'https://restcountries.com/v3.1/all';
+
+  async function fetchApi() {
     try {
-      let res=await fetch(API_ENDPOINT);
-      if(!res.ok){
+      let res = await fetch(API_ENDPOINT);
+      if (!res.ok) {
         throw new Error('Network response was not ok');
       }
-      const fetchdata = await res.json();
-      // console.log(data);
-      setData(fetchdata);
+      const fetchData = await res.json();
+      setData(fetchData);
+      setOriginalData(fetchData); 
     } catch (error) {
-      setError(error.message); 
+      console.error("API Error:", error.message);
     }
-  };
-  useEffect(()=>{
+  }
+
+  useEffect(() => {
     fetchApi();
-  },[]);
+  }, []);
 
   function handleChange(e) {
     const searchItem = e.target.value.toLowerCase();
     if (searchItem === '') {
-      fetchApi();
-    }else {
-      const filteredData = data.filter(country =>
+    
+      setData(originalData);
+    } else {
+      
+      const filteredData = originalData.filter(country =>
         country.name.common.toLowerCase().includes(searchItem)
       );
       setData(filteredData);
     }
   }
+
   return (
     <>
-    <div className="input-container">
-        <input type="text" placeholder="Search for countries" className="search" onChange={handleChange}/>
+      <div className="input-container">
+        <input type="text" placeholder="Search for countries" className="search" onChange={handleChange} />
       </div>
-    <div className="countryCard">
+      <div className="countryCard">
         {data.length > 0 ? (
           data.map(({ name, flags, cca3 }) => (
             <div key={cca3} className="country">
-              <img src={flags.svg} alt={name.common} style={{width:'100px', height:'100px'}} />
+              <img src={flags.svg} alt={name.common} style={{ width: '100px', height: '100px' }} />
               <h2>{name.common}</h2>
             </div>
           ))
@@ -51,8 +55,6 @@ function App() {
           <p>No countries found</p>
         )}
       </div>
-    
-    
     </>
   );
 }
