554db11f31f91f08debfbc3ebb16a9d03f542da9
Piyush arya <piyush.arya@letzcreate.in>
2024-11-10 02:37:15 +0530

    XcountriesSearch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './styles.css'
 
 function App() {
+  // let country=[1,2,3,4,5,6,7,8,9]
+  const [data,setData]=useState([]);
+  const [error, setError] = useState(null);
+  const API_ENDPOINT='https://restcountries.com/v3.1/all';
+  async function fetchApi(){
+    try {
+      let res=await fetch(API_ENDPOINT);
+      if(!res.ok){
+        throw new Error('Network response was not ok');
+      }
+      const fetchdata = await res.json();
+      // console.log(data);
+      setData(fetchdata);
+    } catch (error) {
+      setError(error.message); 
+    }
+  };
+  useEffect(()=>{
+    fetchApi();
+  },[]);
+
+  function handleChange(e) {
+    const searchItem = e.target.value.toLowerCase();
+    if (searchItem === '') {
+      fetchApi();
+    }else {
+      const filteredData = data.filter(country =>
+        country.name.common.toLowerCase().includes(searchItem)
+      );
+      setData(filteredData);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="input-container">
+        <input type="text" placeholder="Search for countries" className="search" onChange={handleChange}/>
+      </div>
+    <div className="countryCard">
+        {data.length > 0 ? (
+          data.map(({ name, flags, cca3 }) => (
+            <div key={cca3} className="country">
+              <img src={flags.svg} alt={name.common} style={{width:'100px', height:'100px'}} />
+              <h2>{name.common}</h2>
+            </div>
+          ))
+        ) : (
+          <p>No countries found</p>
+        )}
+      </div>
+    
+    
+    </>
   );
 }
 
